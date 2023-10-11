4a084b2052a6dd755fc367de5eaa1c1f96529769
Nikolas Morado <nikolasmorado@gmail.com>
2023-10-11 18:36:52 -0400

    Move to axios, class, and svelte fixes

--- index.js
+++ index.js
@@ -1,78 +1,110 @@
-import sget from 'simple-get'
+import axios from 'axios';
 
-function loginSteam(opt) {
+class SteamLogin {
+  /**
+   * Create a new LoginSteam instance.
+   * @param {Object} opt - Configuration options.
+   * @param {string} opt.realm - The realm for OpenID authentication.
+   * @param {string} opt.returnUrl - The return URL after authentication.
+   * @param {string} opt.apiKey - Your Steam API key.
+   */
+  constructor(opt) {
     this.realm = opt.realm;
     this.returnUrl = opt.returnUrl;
     this.apiKey = opt.apiKey;
-    
-    this.get_URL_redirect = function () {//url for redirect to steam openid2.0 login site (https://steamcommunity.com/openid/login)
-        let queryParams = {
-            'openid.ns': 'http://specs.openid.net/auth/2.0',
-            'openid.claimed_id': 'http://specs.openid.net/auth/2.0/identifier_select',
-            'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
-            'openid.return_to': this.returnUrl,
-            'openid.realm': this.realm,
-            'openid.mode': 'checkid_setup'
-        }
-        return 'https://steamcommunity.com/openid/login?' + new URLSearchParams(queryParams).toString(); // Get redirect url for Steam
-    }
-    this.get_URL_check_authentication = function (query) {//url for server check that is really authenticate
-        query['openid.mode'] = 'check_authentication';
-        return 'https://steamcommunity.com/openid/login?' + new URLSearchParams(query).toString();
-    }
-    this.get_URL_publicInfo = function (query) {//url for get public info from steam api, need api key from https://steamcommunity.com/dev/apikey 
-        let queryParams = {
-            steamids: query['openid.claimed_id'],
-            key: this.apiKey,
-            format: 'json'
-        }
-        return 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?' + new URLSearchParams(queryParams).toString();
-    }
+  }
 
-    this.is_valid = (query, cbTrue, cbFalse) => {//server send request to steam to check that user is really authenticate
-        sget.concat({
-            url: this.get_URL_check_authentication(query),
-            method: 'GET'
-        }, (err, res, data) => {
-            if (err) {
-                cbFalse(err);
-                return
-            }
-            data.toString().includes("is_valid:true") ? cbTrue() : cbFalse('is_valid: false');
-        })
-    }
+  /**
+   * Generate the URL for initiating the Steam OpenID authentication.
+   * @returns {string} The authentication URL.
+   */
+  getURLRedirect() {
+    const queryParams = new URLSearchParams({
+      'openid.ns': 'http://specs.openid.net/auth/2.0',
+      'openid.claimed_id': 'http://specs.openid.net/auth/2.0/identifier_select',
+      'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
+      'openid.return_to': this.returnUrl,
+      'openid.realm': this.realm,
+      'openid.mode': 'checkid_setup',
+    });
 
-    this.get_publicInfo = (query, cbTrue, cbFalse) => {//server send request to steam api to get public info
-        sget.concat({
-            url: this.get_URL_publicInfo(query),
-            method: 'GET',
-            json: true
-        }, (err, res, data) => {
-            if (err) {
-                cbFalse(err);
-                return
-            }
-            cbTrue(data.response.players[0]);
-        })
-    }
+    return `https://steamcommunity.com/openid/login?${queryParams.toString()}`;
+  }
 
-    this.authenticate = (query, cbTrue, cbFalse) => {//return the info if user is authenticate or errore
-        if(cbTrue === undefined || cbFalse === undefined){
-            return this.authenticatePromise(query);
-        }
-        this.is_valid(
-            query,
-            () => {
-                this.get_publicInfo(query, cbTrue, cbFalse)
-            },
-            cbFalse
-        )
-    }
+  /**
+   * Generate the URL for checking OpenID authentication.
+   * @param {Object} query - The query parameters from the OpenID response.
+   * @returns {string} The URL for checking authentication.
+   */
+  getURLCheckAuthentication(query) {
+    const sp = new URLSearchParams(query.url.searchParams);
+    sp.set('openid.mode', 'check_authentication');
+    return `https://steamcommunity.com/openid/login?${sp}`;
+  }
 
-    this.authenticatePromise = (query) => {//return the info if user is authenticate or error        
-        return new Promise((resolve, reject) => {  
-            this.authenticate(query, resolve, reject)
-        })
+  /**
+   * Generate the URL for retrieving public Steam user information.
+   * @param {Object} query - The query parameters from the OpenID response.
+   * @returns {string} The URL for fetching user information.
+   */
+  getURLPublicInfo(query) {
+    const queryParams = new URLSearchParams({
+      steamids: query['openid.claimed_id'],
+      key: this.apiKey,
+      format: 'json',
+    });
+
+    return `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?${queryParams.toString()}`;
+  }
+
+  /**
+   * Check if the OpenID response is valid.
+   * @param {Object} query - The query parameters from the OpenID response.
+   * @returns {Promise<boolean>} A Promise that resolves to true if valid, or false if not.
+   */
+  isValid(query) {
+    return axios.get(this.getURLCheckAuthentication(query))
+      .then((response) => response.data.includes("is_valid:true"));
+  }
+
+  /**
+   * Retrieve public Steam user information if authentication is valid.
+   * @param {Object} query - The query parameters from the OpenID response.
+   * @returns {Promise<Object>} A Promise that resolves to the user information.
+   */
+  getPublicInfo(query) {
+    return axios.get(this.getURLPublicInfo(query))
+      .then((response) => response.data.response.players[0]);
+  }
+
+  /**
+   * Authenticate the user with Steam OpenID.
+   * @param {Object} query - The query parameters from the OpenID response.
+   * @returns {Promise<Object>} A Promise that resolves to the user information if authentication is successful.
+   * @throws {Error} Throws an error with the message 'is_valid: false' if authentication fails.
+   */
+  async authenticate(query) {
+    try {
+      const valid = await this.isValid(query);
+      if (valid) {
+        return this.getPublicInfo(query);
+      } else {
+        throw new Error('is_valid: false');
+      }
+    } catch (error) {
+      throw error;
     }
+  }
+
+  /**
+   * Authenticate the user with Steam OpenID using Promises.
+   * @param {Object} query - The query parameters from the OpenID response.
+   * @returns {Promise<Object>} A Promise that resolves to the user information if authentication is successful.
+   * @throws {Error} Throws an error with the message 'is_valid: false' if authentication fails.
+   */
+  authenticatePromise(query) {
+    return this.authenticate(query);
+  }
 }
-export default loginSteam;
+
+export default LoginSteam;
--- readme.md
+++ readme.md
@@ -1,37 +1,54 @@
-Module for make a login with steam credentials
+# SvelteKit Steam Login
 
-# How to use
-Import module
+SvelteKit JavaScript library that simplifies Steam OpenID authentication and allows you to retrieve public Steam user information.
 
-```
-import loginSteam from 'simple-steam-login'
-```
+## Installation
 
-Generate steam api key on https://steamcommunity.com/dev/apikey
+You can install the library using npm or yarn:
 
-Create object login steam
-
-```
-const port = 3000;
-const STEAM_API = `<your_api_key>`;
-const STEAM_CALLBACK_URI = `http://localhost:${port}/login/callback`;
-const REALM = `http://localhost`;
-const opt = {
-    realm: REALM, // Site name displayed to users on logon
-    returnUrl: STEAM_CALLBACK_URI, // Your return route
-    apiKey: STEAM_API //Steam API key   
-}
-const login = new loginSteam(opts)
+```bash
+npm install sveltekit-login-steam
 ```
 
-In your route redirect to this url
-```
-const url = login.get_URL_redirect()
+or
+
+```bash
+yarn add sveltekit-login-steam
 ```
 
-In your callback route (STEAM_CALLBACK_URI) get user info by passing query params object to authenticate() method
-```
-const data = await login.authenticate(request.query);
+## Usage
+
+To use the `Steam Login` library, you'll need to import it and create an instance with your configuration options. Here's a basic example of how to use it in a SvelteKit project:
+
+```javascript
+import Steam Login from 'sveltekit-login-steam';
+
+// Create a new instance with your Steam API key and authentication settings.
+const steamAuth = new Steam Login({
+  realm: 'https://yourwebsite.com',
+  returnUrl: 'https://yourwebsite.com/auth/steam/callback',
+  apiKey: 'your-steam-api-key',
+});
+
+// Generate the URL for initiating Steam OpenID authentication.
+const authURL = steamAuth.getURLRedirect();
+console.log('Authentication URL:', authURL);
+
+// Handle the OpenID response and authenticate the user.
+// You can use Promises or async/await for this step.
 ```
 
-data cointains all public user info
+For more details on available methods and how to handle the OpenID response, please refer to the library's [API documentation](#api-documentation).
+
+## API Documentation
+
+| Method                             | Description                                                        |
+| ---------------------------------- | ------------------------------------------------------------------ |
+| `constructor(opt)`                 | Create a new `Steam Login` instance.                               |
+| `getURLRedirect()`                 | Generate the URL for initiating Steam OpenID authentication.       |
+| `getURLCheckAuthentication(query)` | Generate the URL for checking OpenID authentication.               |
+| `getURLPublicInfo(query)`          | Generate the URL for retrieving public Steam user information.     |
+| `isValid(query)`                   | Check if the OpenID response is valid.                             |
+| `getPublicInfo(query)`             | Retrieve public Steam user information if authentication is valid. |
+| `authenticate(query)`              | Authenticate the user with Steam OpenID.                           |
+| `authenticatePromise(query)`       | Authenticate the user with Steam OpenID using Promises.            |
